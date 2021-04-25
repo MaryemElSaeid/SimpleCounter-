307451fce29ca9d882eb5ab328aadee80c60f3fa
MaryemElSaeid <maryemelsaeid@gmail.com>
2021-04-25 16:35:38 +0200

    Intial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+ render(){
+    return (
+      <div><Counter initCount={3}/></div>
+
+    );
+ }
 }
 
+
+class Counter extends React.Component {
+  constructor(props){
+    super();
+    this.state={
+      count:props.initCount
+    }
+  }
+
+  changeCount=(op)=>{
+    if(op=="+"){
+      this.setState({count:this.state.count+1});
+    } else if (op=="-") {
+      this.setState({count:this.state.count-1});
+    }
+  }
+  
+  render(){
+    return (
+      <div>
+        <div>{this.state.count}</div>
+        <button className="btn btn-success" onClick={()=>this.changeCount("+")}>Increase</button>
+        <button onClick={()=>this.changeCount("-")}>Decrease</button>
+      </div>
+    )
+  }
+}
+
+
+
 export default App;
+
